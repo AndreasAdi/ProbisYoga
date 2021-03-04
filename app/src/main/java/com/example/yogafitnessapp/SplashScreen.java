570840640acea1aa9d5ed3fa65bee38da7229f0f
andre <andreas1@mhs.stts.edu>
2021-03-04 21:42:21 +0700

    tambah SplashScreen

---
+++ app/src/main/java/com/example/yogafitnessapp/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.yogafitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,Home.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+
+    }
+}
